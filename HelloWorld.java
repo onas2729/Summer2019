8e145737f20599dbb7b00394bdc38e2991c24dbf
onas2729 <54293667+onas2729@users.noreply.github.com>
2019-09-06 20:04:54 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Day01_FirstProgramming;
+
+public class HelloWorld {
+	
+	public static void main (String[] args){
+
+	       System.out.println("Hello Osman");
+	       System.out.println("Hello Osman");
+	       System.out.println("Hello Osman");
+	}
+
+}
