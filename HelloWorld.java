a462de3b6725f16fba3c6a606dafb89602fe3cc2
onas2729 <54293667+onas2729@users.noreply.github.com>
2019-09-06 20:05:42 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	       System.out.println("Hello Osman");
 	       System.out.println("Hello Osman");
 	       System.out.println("Hello Osman");
+		System.out.println("Hello");
 	}
 
 }
